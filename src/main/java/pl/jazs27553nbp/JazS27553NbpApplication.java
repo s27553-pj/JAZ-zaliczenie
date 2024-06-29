f295f5fb2068affb9ca123cfe21d0ae4a311a14e
s27553-pj <s27553@pjwstk.edu.pl>
2024-06-29 17:43:16 +0200

    stworzenie projektu, dodanie dependencji, plik properties

---
+++ src/main/java/pl/jazs27553nbp/JazS27553NbpApplication.java
@@ -0,0 +1,13 @@
+package pl.jazs27553nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS27553NbpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JazS27553NbpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/jazs27553nbp/JazS27553NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jazs27553nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS27553NbpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
